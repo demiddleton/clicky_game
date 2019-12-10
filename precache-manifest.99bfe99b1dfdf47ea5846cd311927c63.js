063be55bb2e527d355f484ea19da948509cdea66
Denise Middleton <demiddleton@bellsouth.net>
2019-12-09 20:23:35 -0500

    Updates

---
+++ precache-manifest.99bfe99b1dfdf47ea5846cd311927c63.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1e56525ddb625097622931cea4b9b8a",
+    "url": "/clicky_game/index.html"
+  },
+  {
+    "revision": "58eb973fe01290739b28",
+    "url": "/clicky_game/static/css/main.a0b6ea6a.chunk.css"
+  },
+  {
+    "revision": "3587ac43ad94d73ab92c",
+    "url": "/clicky_game/static/js/2.736949c7.chunk.js"
+  },
+  {
+    "revision": "58eb973fe01290739b28",
+    "url": "/clicky_game/static/js/main.326811e0.chunk.js"
+  },
+  {
+    "revision": "9765900e52cdf99fd4b6",
+    "url": "/clicky_game/static/js/runtime-main.323f9d68.js"
+  },
+  {
+    "revision": "c3df1866f499680081788a5d3374c228",
+    "url": "/clicky_game/static/media/image12.c3df1866.jpg"
+  },
+  {
+    "revision": "1043fcda3661bf25aad6f350d46cc453",
+    "url": "/clicky_game/static/media/image13.1043fcda.jpg"
+  },
+  {
+    "revision": "3a32e416bb88d241e714bc4ce198f12f",
+    "url": "/clicky_game/static/media/image15.3a32e416.jpg"
+  },
+  {
+    "revision": "b51a1acff9f9eb69a85c0520a4caa47c",
+    "url": "/clicky_game/static/media/image3.b51a1acf.jpg"
+  },
+  {
+    "revision": "14d640411ae8e7e67cc9f6c3faf3c8f7",
+    "url": "/clicky_game/static/media/image6.14d64041.jpg"
+  },
+  {
+    "revision": "77bbbd5048a827c51f9b39c854b0118b",
+    "url": "/clicky_game/static/media/image9.77bbbd50.jpg"
+  }
+]);
